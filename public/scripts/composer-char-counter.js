e896a67ea55c8c8454463a403528f1a423c00ecc
Dango3010 <thuyoanh199930@gmail.com>
2022-12-28 05:11:04 +0000

    adding composer-char-counter.js to feature the character counter function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
+  $("textarea#tweet-text").on('keydown', function() {
+    const input = $(this).val();
+    const left = 140 - input.length;
+    console.log(input);
+    console.log('left to use:', left);
   
+    $(this).parentsUntil("div").find(".counter").val(left);
+  });
 });
