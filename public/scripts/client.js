0f144cc0d03329b8a56b383428deea23950accc8
Dango3010 <thuyoanh199930@gmail.com>
2023-01-03 04:16:06 +0000

    negative countdown function added, post method to post new tweet and loadTweets functions were also added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,60 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = function(data) {
+  const time = timeago.format(data['created_at']);
+  let $tweet = `
+  <article id='single-tweet'>
+    <header>
+      <span>
+        <i class="fas fa-skull"></i>
+        <span style='color:grey; padding-left: 10px;'>${data['user'].name}</span>
+      </span>
+      <span style='color: lightgrey;'>${data['user']['handle']}</span>
+    </header>
+    <br>
+    <footer>
+      <textarea>${data['content'].text}</textarea>
+    </footer>
+    <div>
+      <span>${time}</span>
+      <span>
+        <span class="icon"><i class="fa-brands fa-font-awesome"></i></span>
+        <span class="icon" style='padding-left:3px;'><i class="fas fa-retweet"></i></span>
+        <span class="icon" style='padding-left:3px;'><i class="fa-solid fa-heart"></i></span>
+      </span>
+    </div>
+  </article>
+  <br>
+  `; //create the tweet element
+  return $tweet;
+};
+
+const renderTweets = function(tweets) {
+  for (let item of tweets) {
+    const $each = createTweetElement(item)
+    console.log($each);
+    $(document).ready(function(){
+      $('#tweets-container').append($each);
+    });
+  }
+};
+
+//form submission
+$(document).ready(function(){
+  $("form").submit(function(event) {
+    event.preventDefault();
+    const formValues = $(this).serialize(); 
+    console.log(formValues);
+    $.post('/tweets', formValues)
+    .then(loadTweets)
+  });
+});
+
+//get request func
+const loadTweets = function(){
+  $.get('/tweets', function(data){
+    console.log('Success: ', data);
+    renderTweets(data);
+  })
+}
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
-  $("textarea#tweet-text").on('keydown', function() {
-    const input = $(this).val();
-    const left = 140 - input.length;
+  $("#tweet-text").on('input', function() {
+    const input = $(this).val().length;
+    const left = 140 - input;
     console.log(input);
     console.log('left to use:', left);
   
-    $(this).parentsUntil("div").find(".counter").val(left);
+    $(this).siblings("div").children(".counter").text(left);
+    if (input > 140) {
+      $(function(){
+        $("output").addClass("negative");
+      });
+    } else {
+      $("output").removeClass("negative");
+    }
   });
 });
