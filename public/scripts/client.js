fe396b255b54b21e52ca06e74170f88f27e17ec3
Dango3010 <thuyoanh199930@gmail.com>
2023-01-04 15:17:19 +0000

    improve <article> element and its css, secure against untrusted texts with escape function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,61 +3,76 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function () {
 
-const createTweetElement = function(data) {
-  const time = timeago.format(data['created_at']);
-  let $tweet = `
+  const createTweetElement = function (data) {
+    const escape = function (str) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    }; //not interpreted (triggering, having effect as JS), but renders the string as normal text.
+    const time = timeago.format(data['created_at']);
+    let tweet = `
   <article id='single-tweet'>
     <header>
       <span>
-        <i class="fas fa-skull"></i>
-        <span style='color:grey; padding-left: 10px;'>${data['user'].name}</span>
+        <img src="${data['user'].avatars}">
+        <span style='color:grey;'>${data['user'].name}</span>
       </span>
       <span style='color: lightgrey;'>${data['user']['handle']}</span>
     </header>
     <br>
+    <p class='text' style='margin-top: 11px;'>
+      <span>${escape(data['content'].text)}</span>
+    </p>
     <footer>
-      <textarea>${data['content'].text}</textarea>
-    </footer>
-    <div>
-      <span>${time}</span>
-      <span>
+      <span style='padding-left: 7px; font-size: 15px;'>${time}</span>
+      <div>
         <span class="icon"><i class="fa-brands fa-font-awesome"></i></span>
         <span class="icon" style='padding-left:3px;'><i class="fas fa-retweet"></i></span>
         <span class="icon" style='padding-left:3px;'><i class="fa-solid fa-heart"></i></span>
-      </span>
-    </div>
+      </div>
+    </footer>
   </article>
   <br>
-  `; //create the tweet element
-  return $tweet;
-};
+  `;
+    return tweet;
+  };
 
-const renderTweets = function(tweets) {
-  for (let item of tweets) {
-    const $each = createTweetElement(item)
-    console.log($each);
-    $(document).ready(function(){
+  const renderTweets = function (tweets) {
+    $('#tweets-container').empty();
+    for (let i = tweets.length - 1; i >= 0; i--) {
+      const $each = createTweetElement(tweets[i]);
       $('#tweets-container').append($each);
-    });
+    }
+  };
+
+  //get request func
+  const loadTweets = function () {
+    $.get('/tweets', function (data) {
+      console.log('Success: ', data);
+      renderTweets(data);
+    })
   }
-};
 
-//form submission
-$(document).ready(function(){
-  $("form").submit(function(event) {
+  //form submission
+  $("form").submit(function (event) {
     event.preventDefault();
-    const formValues = $(this).serialize(); 
-    console.log(formValues);
-    $.post('/tweets', formValues)
-    .then(loadTweets)
+    const formValues = $(this).serialize();
+    const input = $('#tweet-text').val();
+    console.log(input);
+    if (!input) {
+      alert('the tweet is empty');
+    } else if (input.length > 140) {
+      $('.new-tweet p').addClass('show');
+    } else {
+      $.post('/tweets', formValues)
+        .then(() => {
+          loadTweets();
+          this.reset();
+        })
+    }
   });
-});
 
-//get request func
-const loadTweets = function(){
-  $.get('/tweets', function(data){
-    console.log('Success: ', data);
-    renderTweets(data);
-  })
-}
+  loadTweets() //to post 2 default user tweets first
+});
